c5b1dfa911b2a127ab445cbb8fb22150eeb143aa
bobbyprabowo <prabowo.bobby@gmail.com>
2020-10-17 20:30:47 +0700

    improve url filters

--- src/main/kotlin/com/bobbyprabowo/kontribute/filter/ActorFilter.kt
+++ src/main/kotlin/com/bobbyprabowo/kontribute/filter/ActorFilter.kt
@@ -3,15 +3,12 @@ package com.bobbyprabowo.kontribute.filter
 class ActorFilter : UrlFilter {
 
     override fun execute(urlList: List<String>): List<String> {
-        return urlList.filter { url ->
-            url.contains("@quipper.com", true) ||
-                    url.contains("@gamalinda.com", true)
-        }.map { url ->
-            url.replace("@quipper.com", "", true)
-                .replace("@gamalinda.com", "", true)
-                .replace("http://href=mailto:", "", true)
-                .replace("http://", "", true)
-                .replace("/", "", true)
-        }
+        return urlList
+            .filter { url ->
+                url.contains("@")
+            }
+            .map {url ->
+                url.split("@").first()
+            }
     }
 }
--- src/main/kotlin/com/bobbyprabowo/kontribute/filter/UrlForDisplayFilter.kt
+++ src/main/kotlin/com/bobbyprabowo/kontribute/filter/UrlForDisplayFilter.kt
@@ -3,9 +3,7 @@ package com.bobbyprabowo.kontribute.filter
 class UrlForDisplayFilter : UrlFilter {
 
     override fun execute(urlList: List<String>): List<String> {
-        return urlList.distinct().filter { url ->
-            !url.contains("@quipper.com", true)
-        }.filter {url ->
+        return urlList.distinct().filter {url ->
             url.contains("https://github.com/quipper/", true)
         }
     }
